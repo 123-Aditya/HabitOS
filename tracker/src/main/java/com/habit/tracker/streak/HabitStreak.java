85d7a2fe107290bbf44418bf4e5d68774b10b421
123-Aditya <mraditya345@gmail.com>
2026-01-10 16:08:29 +0530

    Fixing caching issues for reddis caching

--- tracker/src/main/java/com/habit/tracker/streak/HabitStreak.java
+++ tracker/src/main/java/com/habit/tracker/streak/HabitStreak.java
@@ -2,9 +2,11 @@ package com.habit.tracker.streak;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class HabitStreak {
     private int currentStreak;
     private int longestStreak;
