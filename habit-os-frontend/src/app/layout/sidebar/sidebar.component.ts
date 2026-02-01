ca47325a5ca8c67266865272ed3fc3689d38a1a9
123-Aditya <mraditya345@gmail.com>
2026-02-01 13:31:45 +0530

    Adding routing in components

--- habit-os-frontend/src/app/layout/sidebar/sidebar.component.ts
+++ habit-os-frontend/src/app/layout/sidebar/sidebar.component.ts
@@ -3,13 +3,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
-  imports: [MatListModule, MatIconModule, RouterModule]
+  imports: [MatListModule, MatIconModule, RouterModule, RouterLink,
+    RouterLinkActive]
 })
 export class SidebarComponent {
 
