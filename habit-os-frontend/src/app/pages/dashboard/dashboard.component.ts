74b4e03dcbcaf21850d0ed9566f39db5186be019
123-Aditya <mraditya345@gmail.com>
2026-02-11 19:53:40 +0530

    Adding Dashboard page logic

--- habit-os-frontend/src/app/pages/dashboard/dashboard.component.ts
+++ habit-os-frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  constructor(private http: HttpClient) {}
+
+  data: any;
+  loading = true;
+
+  totalHabits = 0;
+  totalEntries = 0;
+
+  ngOnInit(): void {
+    const today = new Date().toISOString().split('T')[0];
+
+    this.http.get<any>(
+      `http://localhost:8080/api/habits/progress/bulk?from=${today}&to=${today}`
+    ).subscribe(res => {
+
+      this.data = res;
+
+      if (res?.habits) {
+        this.totalHabits = res.habits.length;
+
+        this.totalEntries = res.habits.reduce(
+          (sum: number, h: any) => sum + (h.progress?.length || 0),
+          0
+        );
+      }
+
+      this.loading = false;
+    });
+  }
 }
