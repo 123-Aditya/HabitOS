2c1b74b9239b008a136a5e13ddc172ca2d712ed1
123-Aditya <mraditya345@gmail.com>
2026-02-11 20:12:22 +0530

    Adding startdate and enddate input for dashboard page

--- habit-os-frontend/src/app/pages/dashboard/dashboard.component.ts
+++ habit-os-frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
@@ -20,30 +21,53 @@ export class DashboardComponent implements OnInit {
   totalEntries = 0;
   completionPercentage = 0;
 
+  fromDate!: string;
+  toDate!: string;
+
   ngOnInit(): void {
-    const today = new Date().toISOString().split('T')[0];
+      const today = new Date().toISOString().split('T')[0];
 
-    this.http.get<any>(
-      `http://localhost:8080/api/habits/progress/bulk?from=${today}&to=${today}`
-    ).subscribe(res => {
+      this.fromDate = today;
+      this.toDate = today;
 
+      this.loadDashboard();
+  }
+
+loadDashboard(): void {
+
+  if (!this.fromDate || !this.toDate) 
+    return;
+
+  this.loading = true;
+
+  this.http.get<any>(
+    `http://localhost:8080/api/habits/progress/bulk?from=${this.fromDate}&to=${this.toDate}`
+  ).subscribe({
+    next: (res) => {
       this.data = res;
 
       if (res?.habits) {
-          this.totalHabits = res.habits.length;
+        this.totalHabits = res.habits.length;
+        this.totalEntries = res.habits.reduce(
+          (sum: number, h: any) => sum + (h.progress?.length || 0),
+          0
+        );
 
-          this.totalEntries = res.habits.reduce(
-            (sum: number, h: any) => sum + (h.progress?.length || 0),
-              0
-          );
-
-          if (this.totalHabits > 0) {
-            this.completionPercentage =
+        if (this.totalHabits > 0) {
+          this.completionPercentage =
             Math.round((this.totalEntries / this.totalHabits) * 100);
-          }
+        } else {
+          this.completionPercentage = 0;
+        }
       }
 
       this.loading = false;
-    });
-  }
+    },
+    error: (err) => {
+      console.error(err);
+      this.loading = false;
+    }
+  });
+}
+
 }
