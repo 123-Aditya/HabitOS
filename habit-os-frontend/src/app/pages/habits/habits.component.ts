ffa5cf89e5ed9e661cf352aebf9eab8340509c10
123-Aditya <mraditya345@gmail.com>
2026-02-12 19:19:00 +0530

    Adding logic for habits page

--- habit-os-frontend/src/app/pages/habits/habits.component.ts
+++ habit-os-frontend/src/app/pages/habits/habits.component.ts
@@ -1,11 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-habits',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './habits.component.html',
-  styleUrl: './habits.component.css'
+  styleUrls: ['./habits.component.css']
 })
-export class HabitsComponent {
+export class HabitsComponent implements OnInit {
 
+  habits: any[] = [];
+  loading = true;
+
+  newHabit = {
+    name: '',
+    frequency: 'DAILY',
+    targetCount: 1
+  };
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadHabits();
+  }
+
+  loadHabits() {
+    this.http.get<any[]>('http://localhost:8080/api/habits')
+      .subscribe({
+        next: (res) => {
+          console.log("Habits:", res);
+          this.habits = res;
+          this.loading = false;
+        },
+        error: (err) => {
+          console.error("Error loading habits:", err);
+          this.loading = false;
+        }
+      });
+  }
+
+  addHabit() {
+    if (!this.newHabit.name) return;
+
+    this.http.post('http://localhost:8080/api/habits', this.newHabit)
+      .subscribe(() => {
+        this.newHabit = {
+          name: '',
+          frequency: 'DAILY',
+          targetCount: 1
+        };
+        this.loadHabits();
+      });
+  }
+
+  deleteHabit(id: number) {
+    this.http.delete(`http://localhost:8080/api/habits/${id}`)
+      .subscribe(() => this.loadHabits());
+  }
+
+  completeHabit(id: number) {
+    this.http.post(
+      `http://localhost:8080/api/habits/${id}/complete`,
+      {}
+    ).subscribe(() => {
+      alert("Marked completed!");
+    });
+  }
 }
